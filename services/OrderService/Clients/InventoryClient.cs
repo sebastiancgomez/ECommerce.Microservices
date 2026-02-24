bae5c13ed99f80023fa9a610032588fdd5b43825
sebastiancgomez <sebastian.cgomez@gmail.com>
2026-02-24 16:09:26 -0500

    Integrate OrderService with Inventory and Pricing using HttpClientFactory

---
+++ services/OrderService/Clients/InventoryClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http.Json;
+
+namespace OrderService.Clients;
+
+public class InventoryClient: IInventoryClient
+{
+    private readonly HttpClient _httpClient;
+
+    public InventoryClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<bool> IsAvailable(int productId, int quantity)
+    {
+        return await _httpClient.GetFromJsonAsync<bool>(
+            $"api/Inventory/{productId}/{quantity}");
+    }
+
+    public async Task Reserve(int productId, int quantity)
+    {
+        await _httpClient.GetAsync(
+            $"api/Inventory/Reserve/{productId}/{quantity}");
+    }
+}
